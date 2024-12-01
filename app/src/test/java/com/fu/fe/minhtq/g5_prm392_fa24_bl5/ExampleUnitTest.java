4420e2e3542a6d2511c9415db03be56ba3553af1
hoanglv <hoanglvhe170280@fpt.edu.vn>
2024-12-01 22:14:49 +0700

    first commit

---
+++ app/src/test/java/com/fu/fe/minhtq/g5_prm392_fa24_bl5/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.fu.fe.minhtq.g5_prm392_fa24_bl5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
